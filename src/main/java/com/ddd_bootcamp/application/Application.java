cb4686f74a093426ecaf9d8bfb220b6ab526261e
Sarah Brinckmann <sarah.brinckmann@thoughtworks.com>
2023-06-12 15:26:57 +0200

    problem 4

--- src/main/java/com/ddd_bootcamp/application/Application.java
+++ src/main/java/com/ddd_bootcamp/application/Application.java
@@ -8,8 +8,12 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         Cart cart = new Cart();
-        Product product = new Product("Apple Pencil");
-        cart.add(product);
+        Product applePencil = new Product("Apple Pencil");
+        Product headPhone = new Product("Sony Wireless headphone");
+        cart.add(applePencil, 2);
+        cart.add(headPhone, 1);
+
+        cart.remove(applePencil);
 
         System.out.println("Cart = " + cart);
         List<Product> products = cart.getProducts();
--- src/main/java/com/ddd_bootcamp/domain/Cart.java
+++ src/main/java/com/ddd_bootcamp/domain/Cart.java
@@ -1,23 +1,36 @@
 package com.ddd_bootcamp.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Cart {
-    private List<Product> products = new ArrayList<>();
+    private List<Product> removedProducts = new ArrayList<>();
+    private Map<Product, Integer> items = new HashMap();
 
-    public void add(Product product) {
-        products.add(product);
+    public void add(Product product, int quantity) {
+        items.put(product, quantity);
+    }
+
+    public void remove(Product product) {
+        items.remove(product);
+        removedProducts.add(product);
+    }
+
+    public List<Product> getRemovedProducts() {
+        return removedProducts;
     }
 
     public List<Product> getProducts() {
-        return products;
+        return items.keySet().stream().toList();
     }
 
     @Override
     public String toString() {
         return "Cart{" +
-                "products=" + products +
+                "products=" + items.keySet().stream().toList() +
+                "removed products = " + removedProducts +
                 '}';
     }
 }
